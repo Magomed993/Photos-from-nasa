42b63e91dccb26ae1c1c6ffbfa818d9a0595b320
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-05-12 14:15:50 +0300

    publish text to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,11 @@
+import telegram
+import os
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    telega_api = os.environ['TELEGA_API']
+    chat_id = os.environ['CHAT_ID']
+    bot = telegram.Bot(token=telega_api)
+    bot.send_message(chat_id=chat_id, text="Привет, Ку-Ку.")
