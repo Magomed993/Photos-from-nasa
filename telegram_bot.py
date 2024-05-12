eaef0ceee84d29db13e1d9209209ed6e176cbc33
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-05-12 23:17:54 +0300

    sending pictures

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,35 @@
 import telegram
 import os
+import time
+import argparse
+import random
 from dotenv import load_dotenv
+
+
+def sending_photos(api, ch_id, arg=None):
+    if arg is None:
+        bot = telegram.Bot(token=api)
+        directory = 'images/'
+        files = os.listdir(directory)
+        random_files = random.choice(files)
+        bot.send_document(chat_id=ch_id, document=open(f'{directory}{random_files}', 'rb'))
+    else:
+        bot = telegram.Bot(token=api)
+        bot.send_document(chat_id=ch_id, document=open(f'images/{arg}', 'rb'))
 
 
 if __name__ == '__main__':
     load_dotenv()
+    os.makedirs('images', exist_ok=True)
     telega_api = os.environ['TELEGA_API']
     chat_id = os.environ['CHAT_ID']
-    bot = telegram.Bot(token=telega_api)
-    bot.send_message(chat_id=chat_id, text="Привет, Ку-Ку.")
-    bot.send_document(chat_id=chat_id, document=open('images/hubble.jpeg', 'rb'))
-    
+    seconds = int(os.getenv('TIME', 14400))
+    parse = argparse.ArgumentParser(description='Описание программы')
+    parse.add_argument('-n', '--name', help='photo name')
+    args = parse.parse_args()
+    try:
+        while True:
+            sending_photos(telega_api, chat_id, args.name)
+            time.sleep(seconds)
+    except FileNotFoundError:
+        print('Введите правильно наименование картинки или формат')
