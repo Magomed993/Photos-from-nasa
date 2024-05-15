09188138b25c9d3953d60422095a627f42eb2414
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-05-15 21:41:10 +0300

    Corrected the variable name

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -8,8 +8,8 @@ def fetch_spacex_last_launch(arg):
     url = 'https://api.spacexdata.com/v5/launches/{0}'.format(arg)
     response = requests.get(url)
     response.raise_for_status()
-    departure_image = response.json()['links']['flickr']['original']
-    for number, picture in enumerate(departure_image):
+    departure_images = response.json()['links']['flickr']['original']
+    for number, picture in enumerate(departure_images):
         path = 'images/spacex_{0}.jpeg'.format(number)
         download_files(picture, path)
 
